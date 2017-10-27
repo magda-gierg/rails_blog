0d0e15a08588e6a8149e0e9dedcf2959a0ababf7
magda-gierg <mjgiergiczny@gmail.com>
2017-10-27 13:48:45 +1300

    delete is working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
   def show
-   @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
@@ -14,7 +15,7 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def create
     @article = Article.new(article_params)
 
@@ -35,9 +36,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-
 end
